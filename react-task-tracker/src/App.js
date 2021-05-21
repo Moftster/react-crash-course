eaf325701609259a9d706efd7b9fb560986aa88b
dmoftakhar <david@chillaweb.com>
2021-05-21 17:53:46 +0100

    Created Header and Button components.

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -2,12 +2,10 @@
 import Header from './components/Header'
 
 function App() {
-  const name = 'Dave'
-  const x = true
 
   return (
     <div className="container">
-      <Header/>
+      <Header />
     </div>
   );
 }
